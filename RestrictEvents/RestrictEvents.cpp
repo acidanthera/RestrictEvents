981aba34939910625ffdd332dd4a1b900a818aff
Andres ZeroCross <ultimate.lancaster@gmail.com>
2024-08-05 18:07:03 +0700

    Added constants for macOS 15 support (#16)

--- RestrictEvents/RestrictEvents.cpp
+++ RestrictEvents/RestrictEvents.cpp
@@ -501,7 +501,7 @@ PluginConfiguration ADDPR(config) {
 	bootargBeta,
 	arrsize(bootargBeta),
 	KernelVersion::MountainLion,
-	KernelVersion::Sonoma,
+	KernelVersion::Sequoia,
 	[]() {
 		DBGLOG("rev", "restriction policy plugin loaded");
 		verboseProcessLogging = checkKernelArgument("-revproc");
